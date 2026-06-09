d0a5984da200618dbd2934ba61e31a744d8d5b6f
kedama-t <114888622+kedama-t@users.noreply.github.com>
2026-06-09 23:07:29 +0900

    chore: favicon/アバターアイコン追加

--- packages/web/app/root.tsx
+++ packages/web/app/root.tsx
@@ -55,6 +55,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Minakata</title>
+        <link rel="icon" href="/agents/mimy.png" type="image/png" />
         <Meta />
         <Links />
       </head>
